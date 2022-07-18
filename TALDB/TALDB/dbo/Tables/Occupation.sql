3b3c2a8abc65285e8fa768c8ff109d08d9606f2f
Shalini Ratnam <shalini.ratnam@sonichealthcare.com.au>
2022-07-18 22:06:04 +1000

    added DB project

---
+++ TALDB/TALDB/dbo/Tables/Occupation.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Occupation] (
+    [OccupationID]       INT           IDENTITY (1, 1) NOT NULL,
+    [Name]               VARCHAR (100) NULL,
+    [OccupationFactorID] INT           NULL,
+    PRIMARY KEY CLUSTERED ([OccupationID] ASC),
+    FOREIGN KEY ([OccupationFactorID]) REFERENCES [dbo].[OccupationFactor] ([OccupationFactorID])
+);
---
+++ TALDB/TALDB/dbo/Tables/OccupationFactor.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[OccupationFactor] (
+    [OccupationFactorID] INT           IDENTITY (1, 1) NOT NULL,
+    [Name]               VARCHAR (100) NULL,
+    [Factor]             FLOAT (53)    NULL,
+    PRIMARY KEY CLUSTERED ([OccupationFactorID] ASC)
+);
+
